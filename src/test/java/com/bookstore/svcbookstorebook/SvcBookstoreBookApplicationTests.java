e8697d1a3b215cbe3d051ddc37f68fb0e1073d5b
svogeti <sameervogeti@gmail.com>
2020-09-27 00:35:04 +0530

    Initial Structure Commit

---
+++ src/test/java/com/bookstore/svcbookstorebook/SvcBookstoreBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.svcbookstorebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcBookstoreBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
